af30897b53a51b749d602a648e000fa0eda74f43
Summer Long <91840911+sumslong@users.noreply.github.com>
2024-03-04 16:11:10 -0600

    updating app

--- matching_app/app.py
+++ matching_app/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from helpers.sentence_matcher import rank_matches
+from helpers.finetuned_generator import generate_text
 from helpers.toxicity import detox
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def home():
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/choice')
+def choice():
+    return render_template('choice.html')
+
+@app.route('/existingmatches')
+def existing_matches():
+    return render_template('existingmatches.html')
+
+@app.route('/realusers', methods=['GET', 'POST'])
+def real_users():
     if request.method == "POST":
         gender = request.form.get('gender')
         if gender == "female":
@@ -24,21 +36,34 @@ def home():
             age_low, age_high = None, None
         biography = request.form['biography']
 
-        toxic_score = detox(biography)
-        print('toxic_score')
-        if toxic_score['severe_toxicity'] > .1 or toxic_score['threat'] > .1:
-            bio_list = ['Try to be less toxic.']
+        if biography != "":
+            bio_list = rank_matches(biography, pref_gender=gender_match, pref_age_lower=age_low, pref_age_higher=age_high)
         else:
-        
-            if biography != "":
-                bio_list = rank_matches(biography, pref_gender = gender_match, pref_age_lower=age_low, pref_age_higher=age_high)
-            else:
-                bio_list = ["Write something"]
+            bio_list = ["Write something"]
     else:
         gender = ''
         age = ''
         biography = ''
         bio_list = []
 
-    return render_template('form.html', results=bio_list, gender=gender, age=age, biography=biography)
-    
+    return render_template('realusers.html', results=bio_list, gender=gender, age=age, biography=biography)
+
+
+@app.route('/generatedusers', methods=['GET', 'POST'])
+def generated_users():
+    if request.method == "POST":
+        biography = request.form['biography']
+        if biography != "":
+            bio_list = generate_text(biography)
+        else:
+            bio_list = ["Write something"]
+    else:
+        gender = ''
+        age = ''
+        biography = ''
+        bio_list = []
+
+    return render_template('generatedusers.html', results=bio_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
